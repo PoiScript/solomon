025d3aed81a2ce2ebe0d38fb6fae9bead0aabd64
PoiScript <poiscript@gmail.com>
2017-01-05 01:14:10 +0800

    Add Kitsu Service

--- src/app/main/home/kitsu/kitsu.component.ts
+++ src/app/main/home/kitsu/kitsu.component.ts
@@ -1,16 +1,32 @@
 import {Component, OnInit} from "@angular/core"
+import {KitsuService} from "./kitsu.service"
+import {Anime} from "./anime"
+import {LibraryEntry} from "./libraryEntry"
 
 @Component({
 	selector: 'kitsu',
 	templateUrl: './kitsu.component.html',
-	styleUrls: ['./kitsu.component.css']
+	styleUrls: ['./kitsu.component.css'],
+	providers: [KitsuService]
 })
 export class KitsuComponent implements OnInit {
+	animes: Anime[]
 
-	constructor() {
+	constructor(private _kitsuService: KitsuService) {
 	}
 
 	ngOnInit() {
+		this._kitsuService.getAnime()
+			.subscribe(
+				data => this.animes = parseKitsuApiJSON(data),
+				err => alert(err),
+				() => console.log('GitHub Repo Refreshed.')
+			)
 	}
+}
 
+function parseKitsuApiJSON(data): Anime[] {
+	let animes: Anime[] = []
+	let libraryEntries: LibraryEntry[]
+	return animes
 }
