32555c3233410c4cd4edc7fcd8f24d10ce3129f2
PoiScript <poiscript@gmail.com>
2017-02-08 22:09:44 +0800

    Added splash screen

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,12 +7,12 @@ const routes: Routes = [
 	{
 		path: '', component: HomeComponent, children: [
 		{path: '', redirectTo: 'recent', pathMatch: 'full'},
-		{path: 'recent', loadChildren: './modules/recent#RecentModule'},
-		{path: 'archive', loadChildren: './modules/archive#ArchiveModule'},
-		{path: 'category/:title', loadChildren: './modules/category#CategoryModule'},
-		{path: 'link', loadChildren: './modules/link#LinkModule'},
-		{path: 'about', loadChildren: './modules/about#AboutModule'},
-		{path: 'search', loadChildren: './modules/search#SearchModule'}
+    {path: 'recent', loadChildren: 'app/modules/recent#RecentModule'},
+    {path: 'archive', loadChildren: 'app/modules/archive#ArchiveModule'},
+    {path: 'category/:title', loadChildren: 'app/modules/category#CategoryModule'},
+    {path: 'link', loadChildren: 'app/modules/link#LinkModule'},
+    {path: 'about', loadChildren: 'app/modules/about#AboutModule'},
+    {path: 'search', loadChildren: 'app/modules/search#SearchModule'}
 	]
 	},
 ]
