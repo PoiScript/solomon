d3aad1219d5ec39822b5edde17fa75547fdcdbcb
PoiScript <poiscript@gmail.com>
2017-02-11 16:34:25 +0800

    Added SolomonConfig

---
+++ src/app/interface/solomon-config.ts
@@ -0,0 +1,9 @@
+export interface SolomonConfig {
+  BLOG_NAME: string,
+  RECENT_POST_LIMIT: number,
+  RECENT_ANIME_LIMIT: number,
+  RECENT_PROJECT_LIMIT: number,
+  GITHUB_USERNAME: string,
+  GITHUB_POST_REPO: string,
+  KITSU_ID: number
+}
