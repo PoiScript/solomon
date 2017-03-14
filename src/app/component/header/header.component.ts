2cca2f0f76a47f1468a4cb40cb5f97682f8d3cc0
PoiScript <poiscript@gmail.com>
2017-03-14 21:38:27 +0800

    refactor: remove ResizeService, use fxShow & fxHide instead

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,15 +1,12 @@
-import {animate, Component, OnInit, state, style, transition, trigger} from '@angular/core'
+import {animate, Component, state, style, transition, trigger} from '@angular/core'
 import {Router} from '@angular/router'
 import {DomSanitizer} from '@angular/platform-browser'
 import {MdIconRegistry} from '@angular/material'
-
-import {ResizeService} from '../../service/resize'
 
 @Component({
   selector: 'solomon-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  providers: [ResizeService],
   animations: [
     trigger('searchBarState', [
       state('true', style({width: '240px'})),
@@ -19,17 +16,12 @@ import {ResizeService} from '../../service/resize'
     ])
   ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   searchBarVisibility: boolean = false
-  isSMLayout: boolean
 
-  constructor(private resizeService: ResizeService,
-              private iconRegistry: MdIconRegistry,
+  constructor(private iconRegistry: MdIconRegistry,
               private sanitizer: DomSanitizer,
               private router: Router) {
-    resizeService.window.subscribe(val => {
-      if (val) this.isSMLayout = val.innerWidth <= 480
-    })
     iconRegistry.addSvgIcon('solomon', sanitizer.bypassSecurityTrustResourceUrl('assets/icon/solomon.svg'))
     iconRegistry.addSvgIcon('about', sanitizer.bypassSecurityTrustResourceUrl('assets/icon/about.svg'))
     iconRegistry.addSvgIcon('archive', sanitizer.bypassSecurityTrustResourceUrl('assets/icon/archive.svg'))
@@ -44,9 +36,4 @@ export class HeaderComponent implements OnInit {
   searchFor(keyword: string): void {
     if (keyword != '') this.router.navigate(['/search', {q: keyword}])
   }
-
-  ngOnInit(): void {
-    this.isSMLayout = window.innerWidth <= 480
-  }
-
 }
