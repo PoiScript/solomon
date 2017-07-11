ece31ebb9eb47db5be016785b3966ed23bc82d7a
PoiScript <poiscript@gmail.com>
2017-07-11 18:17:01 +0800

    chore(build): use public as deploy directory

--- react/scripts/replace-html.js
+++ react/scripts/replace-html.js
@@ -4,7 +4,7 @@ const posts = require('../src/post.json')
 
 posts.forEach(post => {
   const html = fs.readFileSync(`build/post/${post.slug}/index.html`, 'utf8')
-  const content = fs.readFileSync(`public/html/${post.slug}.html`, 'utf8')
+  const content = fs.readFileSync(`../public/html/${post.slug}.html`, 'utf8')
   const $ = cheerio.load(html, {decodeEntities: false})
   $('article').html(content)
   fs.writeFile(`build/post/${post.slug}/index.html`, $.html(), (err) => {
