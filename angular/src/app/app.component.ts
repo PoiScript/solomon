81c84f652aab6ccd4be2c0ce53a771d64a712070
PoiScript <poiscript@gmail.com>
2017-07-15 16:27:05 +0800

    chore: remove angular code and dependencies

--- angular/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-@Component({
-  selector: 'solomon-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-
-export class AppComponent {
-}
