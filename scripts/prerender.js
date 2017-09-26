2382ee756946cc437a2fbbe1b1912553c82f2c4e
PoiScript <poiscript@gmail.com>
2017-09-26 18:59:35 +0800

    refactor(scripts): simplify prerender script

--- scripts/prerender.js
+++ scripts/prerender.js
@@ -1,41 +1,41 @@
 require('zone.js/dist/zone-node')
 require('reflect-metadata')
 const fs = require('fs-extra')
-const path = require('path')
+const {join, resolve} = require('path')
 const {minify} = require('html-minifier')
 const {renderModuleFactory} = require('@angular/platform-server')
 const {provideModuleMap} = require('@nguniversal/module-map-ngfactory-loader')
 
-const assets = path.resolve('assets')
-const dist = path.resolve('dist')
+const dist = resolve('dist')
 const {posts} = require('../solomon.conf')
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require(path.join(dist, 'dist-server/main.bundle'))
-const index = fs.readFileSync(path.join(dist, 'index.html'), 'utf8')
+const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require(join(dist, 'dist-server/main.bundle'))
+const index = fs.readFileSync(join(dist, 'index.html'), 'utf8')
 
-let urls = ['', '/about', '/link']
+let urls = ['/', '/about', '/link']
 
 posts.forEach(post => {
-  urls.push(`/post/${post.slug}`)
-  post.tags.map(tag => urls.push(`/tag/${tag}`))
+  urls.push(join('/post', post.slug))
+  post.tags.map(tag => urls.push(join('/slug', tag)))
 })
 
 urls
   .filter((url, index, self) => self.indexOf(url) === index)
-  .forEach(url => {
-    renderFactory(url)
-      .then(html =>
-        minify(html, {
-          collapseWhitespace: true,
-          removeComments: true,
-          minifyCSS: true
-        }))
-      .then(html => fs.outputFileSync(path.join(dist, url, 'index.html'), html))
-  })
+  .forEach(url => renderToStatic(url, join(dist, url, 'index.html')))
 
-function renderFactory (url) {
-  return renderModuleFactory(AppServerModuleNgFactory, {
+renderToStatic('/404', join(dist, '404.html'))
+
+function renderToStatic (url, path) {
+  renderModuleFactory(AppServerModuleNgFactory, {
     url: url,
     document: index,
     extraProviders: [provideModuleMap(LAZY_MODULE_MAP)]
-  })
+  }).then(html => minify(html, {
+    collapseWhitespace: true,
+    removeComments: true,
+    minifyCSS: true
+  }))
+    .then(html => {
+      console.info(`Saving "${url}" as "${path}"`)
+      fs.outputFileSync(path, html)
+    })
 }
