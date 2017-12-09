1642a48822cdd96e26d95307be6c113521ea4493
PoiScript <poiscript@gmail.com>
2017-12-09 14:20:10 +0800

    feat: add Service Worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from 'app/app.component';
 import { AppRoutingModule } from 'app/app-routing.module';
@@ -11,6 +12,7 @@ import { SharedModule } from 'app/shared';
 
 import { APP_CONFIG } from 'app/app.config';
 import SOLOMON_CONFIG from '../../solomon.conf';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import SOLOMON_CONFIG from '../../solomon.conf';
     BrowserAnimationsModule,
     CoreModule,
     HttpClientModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
     SharedModule,
     PagesModule
   ],
