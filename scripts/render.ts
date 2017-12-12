f787a7054b920922a3e4c70bc2439cbf254c3cd4
PoiScript <poiscript@gmail.com>
2017-12-12 17:02:20 +0800

    feat(scripts): add toc and section block when render markdown

--- scripts/render.ts
+++ scripts/render.ts
@@ -4,13 +4,38 @@ import * as marked from 'marked';
 import { join, resolve } from 'path';
 
 const content = resolve('content');
+const renderer = new marked.Renderer();
+const headings = [];
 
 marked.setOptions({
   highlight: code => require('highlight.js').highlightAuto(code).value
 });
 
+renderer.heading = text => {
+  const id = encodeURI(text);
+  headings.push({id, text});
+  return `</div></section><section id="${id}"><h2>${text}</h2><div class="section-content">`;
+};
+
+const toc = () => `
+  <section class="toc">
+    <h2>${headings.length ? 'Contents' : ''}</h2>
+    <div class="section-content">
+      <nav>
+        <ul>
+          ${headings.map(h => `<li><a href="#${h.id}">${h.text}</a></li>`).join('')}
+        </ul>
+      </nav>
+`;
+
 export function render (post) {
+  // clear headings array
+  while (headings.length) {
+    headings.pop();
+  }
+
   const markdown = readFileSync(join(content, `${post.slug}.md`), 'utf8');
-  const html = marked(markdown);
-  return minify(html, {collapseWhitespace: true});
+  const html = marked(markdown, {renderer: renderer}).concat('</div></section>');
+
+  return minify(toc().concat(html), {collapseWhitespace: true, removeEmptyElements: true});
 }
