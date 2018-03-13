a0f39cf39de780f0c789c1ea50d7d6553b5702ec
PoiScript <poiscript@gmail.com>
2018-03-13 23:32:42 +0800

    refactor: use svg icon instead of icon font
    mat-icon can register and inject inline icon, which means we don't
    have to load the full icon font but load the specific fonts we need.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
-import { MatButtonModule, MatIconModule, MatListModule, MatProgressBarModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatIconRegistry,
+  MatListModule,
+  MatProgressBarModule,
+  MatToolbarModule,
+  MatTooltipModule,
+} from '@angular/material';
 
 import { PostListComponent } from './post-list/post-list.component';
 import { HeaderComponent } from './header/header.component';
@@ -41,4 +50,16 @@ import { SpinnerComponent } from './spinner/spinner.component';
     MatTooltipModule,
   ],
 })
-export class SharedModule {}
+export class SharedModule {
+
+  constructor (private iconRegistry: MatIconRegistry,
+               private sanitizer: DomSanitizer) {
+    this.registerIcon('arrow');
+    this.registerIcon('rss');
+  }
+
+  private registerIcon (name: string) {
+    this.iconRegistry.addSvgIcon(name, this.sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/${name}.svg`));
+  }
+
+}
