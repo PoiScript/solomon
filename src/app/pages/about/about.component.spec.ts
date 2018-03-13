19fedad05eb337efbe297a240a35422f4a844898
PoiScript <poiscript@gmail.com>
2018-03-14 00:38:22 +0800

    fix(about): fix unit test

--- src/app/pages/about/about.component.spec.ts
+++ src/app/pages/about/about.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AboutComponent } from './about.component';
+import { AboutModule } from './about.module';
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -9,7 +11,7 @@ describe('AboutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AboutComponent],
+      imports: [AboutModule, RouterTestingModule],
     })
       .compileComponents();
   }));
