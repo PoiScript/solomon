012d8d03ad0b4c6d82d7cac09a0704a978680eed
PoiScript <poiscript@gmail.com>
2018-03-16 09:25:17 +0800

    build: update to angular-cli 1.7.3 and flex-layout 5.0.0-beta.13

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutServerModule } from '@angular/flex-layout/server';
 import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
@@ -8,6 +9,7 @@ import { AppComponent } from 'app/app.component';
 @NgModule({
   imports: [
     AppModule,
+    FlexLayoutServerModule,
     ServerModule,
     ServerTransferStateModule,
     ModuleMapLoaderModule,
