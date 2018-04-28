be903840e7ebd22f02c6736afbe6e6b1ec778fa5
PoiScript <poiscript@gmail.com>
2018-04-29 01:34:10 +0800

    refactor: cleanup post-resolver and environment
    We're using TransferHttpCacheModule to cache http request, so we
    don't have to manually manage transfer state in the resolver. Also,
    we include AssetInterceptor to interceptor assest request in the
    prior commit so we don't need the environment file for server.

--- src/app/post/post-resolver.service.ts
+++ src/app/post/post-resolver.service.ts
@@ -1,7 +1,5 @@
-import { isPlatformServer } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
-import { makeStateKey, TransferState } from '@angular/platform-browser';
+import { Inject, Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   Resolve,
@@ -11,57 +9,32 @@ import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 
 import { Post, POST_CONFIG, PostConfig, PostResolve } from 'app/shared';
-import { environment } from 'environments/environment';
-
-const RESOLVED_KEY = makeStateKey<PostResolve>('resolved');
 
 @Injectable()
 export class PostResolver implements Resolve<PostResolve> {
-  private posts: Post[];
+  readonly posts: Post[] = this.config.posts;
 
   constructor(
     @Inject(POST_CONFIG) private config: PostConfig,
-    @Inject(PLATFORM_ID) private platformId: {},
-    private state: TransferState,
     private http: HttpClient,
-  ) {
-    this.posts = config.posts;
-  }
+  ) {}
 
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot,
   ): Observable<PostResolve> {
     const slug = route.paramMap.get('slug');
-    const resolved = this.state.get(RESOLVED_KEY, null);
+    const i = this.posts.findIndex(post => post.slug === slug);
 
-    if (resolved && resolved.current.slug === slug) {
-      // if the state exists and matches the slug,
-      // remove it and return null to tell the component do nothing
-      this.state.remove(RESOLVED_KEY);
-      return resolved;
-    } else {
-      const i = this.posts.findIndex(post => post.slug === slug);
-      const next = this.posts[i - 1];
-      const prior = this.posts[i + 1];
-      const current = this.posts[i];
-
-      return this.http
-        .get(`${environment.origin_url}html/${current.slug}.html`, {
-          responseType: 'text',
-        })
-        .pipe(
-          map(html => {
-            const resolve = { current, prior, next, html };
-
-            // only save the state in the server platform
-            if (isPlatformServer(this.platformId)) {
-              this.state.set(RESOLVED_KEY, resolve);
-            }
-
-            return resolve;
-          }),
-        );
-    }
+    return this.http
+      .get(`/html/${slug}.html`, { responseType: 'text' })
+      .pipe(
+        map(html => ({
+          html,
+          next: this.posts[i - 1],
+          prior: this.posts[i + 1],
+          current: this.posts[i],
+        })),
+      );
   }
 }
--- src/environments/environment.server.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  origin_url: 'http://localhost:8080/',
-};
