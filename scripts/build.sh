f7d56913224b6d4aa1599c2044b9e4c241979f34
PoiScript <poiscript@gmail.com>
2018-06-02 23:05:54 +0800

    fix(ci): build application with prod flag

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 ts-node -r tsconfig-paths/register scripts/check
-ng run blog:build
+ng build blog --prod
 ng run blog:server
 ts-node -r tsconfig-paths/register scripts/prerender
