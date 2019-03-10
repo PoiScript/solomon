8c02b270a436f9e9ce83869e8c52f636db1203fa
PoiScript <poiscript@gmail.com>
2019-03-10 12:54:00 +0800

    feat(gen): write rss string directly

--- gen-rs/src/rss.rs
+++ gen-rs/src/rss.rs
@@ -1,42 +1,43 @@
-use std::io::{Result, Write};
+use std::io::Write;
 
 use chrono::Utc;
-use rss::{CategoryBuilder, ChannelBuilder, ItemBuilder};
 
-use crate::Entry;
+use crate::{Entry, render};
+use crate::error::Result;
 
-#[inline]
-pub fn write<W: Write>(w: &mut W, entries: Vec<Entry<'_>>) -> Result<()> {
-    let mut items = Vec::new();
-    for entry in entries {
-        items.push(
-            ItemBuilder::default()
-                .title(Some(String::from(entry.title)))
-                .author(Some("PoiScript".to_owned()))
-                .categories(
-                    entry
-                        .tags
-                        .split_whitespace()
-                        .map(|tag| CategoryBuilder::default().name(tag).build().unwrap())
-                        .collect::<Vec<_>>(),
-                )
-                .description(Some(entry.content))
-                .build()
-                .unwrap(),
-        )
+pub fn write<W: Write>(w: &mut W, entries: &Vec<Entry<'_>>) -> Result<()> {
+    write!(
+        w,
+        "<rss xmlns:dc=\"http://purl.org/dc/elements/1.1/\" \
+         xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" \
+         xmlns:atom=\"http://www.w3.org/2005/Atom\" version=\"2.0\">"
+    )?;
+    write!(w, "<channel>")?;
+    write!(w, "<title>solomon</title>")?;
+    write!(w, "<description>PoiScript's Blog</description>")?;
+    write!(w, "<link>https://blog.poi.cat</link>")?;
+    write!(
+        w,
+        "<generator>solomon {}</generator>",
+        env!("CARGO_PKG_VERSION")
+    )?;
+    write!(
+        w,
+        "<lastBuildDate>{}</lastBuildDate>",
+        Utc::now().to_rfc2822()
+    )?;
+    write!(w, r#"<atom:link href="https://blog.poi.cat/atom.xml" rel="self" type="application/rss+xml"/>"#)?;
+    write!(w, "<language>zh-Hans</language>")?;
+    write!(
+        w,
+        "<copyright>Content licensed under CC-BY-SA-4.0.</copyright>"
+    )?;
+
+    for e in entries {
+        render::rss(e, w)?;
     }
 
-    ChannelBuilder::default()
-        .title("solomon")
-        .link("https://blog.poi.cat")
-        .description("PoiScript's Blog")
-        .language(Some("zh-Hans".to_owned()))
-        .pub_date(Some(Utc::now().to_rfc2822()))
-        .items(items)
-        .build()
-        .unwrap()
-        .write_to(w)
-        .unwrap();
+    write!(w, "</channel></rss>")?;
 
     Ok(())
 }
