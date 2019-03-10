fa9621818e48a740cbf91a9ddf7e4a2a50857fcc
PoiScript <poiscript@gmail.com>
2019-03-10 15:08:38 +0800

    refactor(gen): replace serde_json with json crate

--- gen-rs/src/error.rs
+++ gen-rs/src/error.rs
@@ -1,55 +1,19 @@
-//! Error module
-
-use std::error;
-use std::fmt;
 use std::io::Error as IOError;
 use std::path::PathBuf;
 use std::str::Utf8Error;
 use std::string::FromUtf8Error;
 
 use chrono::ParseError;
-use serde_json::Error as JsonError;
 
 #[derive(Debug)]
 pub enum Error {
     IO(IOError),
     Utf8(Utf8Error),
     Date(ParseError),
-    Json(JsonError),
     MissingTitle(PathBuf),
     MissingDate(PathBuf),
     MissingTags(PathBuf),
     MissingSlug(PathBuf),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> ::std::result::Result<(), fmt::Error> {
-        match self {
-            Error::IO(err) => write!(f, "{}", err),
-            Error::Utf8(err) => write!(f, "{}", err),
-            Error::Date(err) => write!(f, "{}", err),
-            Error::Json(err) => write!(f, "{}", err),
-            Error::MissingTitle(path) => write!(f, "title not found for {:?}", path),
-            Error::MissingDate(path) => write!(f, "date not found for {:?}", path),
-            Error::MissingTags(path) => write!(f, "tags not found for {:?}", path),
-            Error::MissingSlug(path) => write!(f, "slug not found for {:?}", path),
-        }
-    }
-}
-
-impl error::Error for Error {
-    fn cause(&self) -> Option<&error::Error> {
-        match self {
-            Error::IO(err) => Some(err),
-            Error::Utf8(err) => Some(err as &error::Error),
-            Error::Date(err) => Some(err as &error::Error),
-            Error::Json(err) => Some(err as &error::Error),
-            Error::MissingTitle(_) => None,
-            Error::MissingDate(_) => None,
-            Error::MissingTags(_) => None,
-            Error::MissingSlug(_) => None,
-        }
-    }
 }
 
 impl From<IOError> for Error {
@@ -73,12 +37,6 @@ impl From<FromUtf8Error> for Error {
 impl From<ParseError> for Error {
     fn from(err: ParseError) -> Self {
         Error::Date(err)
-    }
-}
-
-impl From<JsonError> for Error {
-    fn from(err: JsonError) -> Self {
-        Error::Json(err)
     }
 }
 
--- gen-rs/src/json.rs
+++ gen-rs/src/json.rs
@@ -1,93 +1,55 @@
 use std::fs::File;
 use std::io::Cursor;
 
-use chrono::NaiveDate;
-use serde::Serialize;
-use serde_json::to_writer;
+use json::{object, JsonValue};
 
 use crate::error::Result;
 use crate::{render, Entry};
 
 pub fn write_summary(entries: &mut Vec<Entry<'_>>) -> Result<()> {
-    #[derive(Serialize)]
-    struct Post<'a> {
-        date: NaiveDate,
-        slug: &'a str,
-        tags: Vec<&'a str>,
-        title: &'a str,
-    }
-
-    to_writer(
-        File::create("assets/posts.json")?,
-        &entries
+    JsonValue::Array(
+        entries
             .iter()
-            .map(|entry| Post {
-                date: entry.date,
-                slug: entry.slug,
-                tags: entry.tags.split_whitespace().collect(),
-                title: entry.title,
+            .map(|entry| object! {
+                "date" => entry.date.format("%F").to_string(),
+                "slug" => entry.slug,
+                "title" => entry.title,
+                "tags" => entry.tags.split_whitespace().map(|t| t.into()).collect::<Vec<JsonValue>>(),
             })
             .collect::<Vec<_>>(),
-    )?;
+    )
+    .write(&mut File::create("assets/posts.json")?)?;
 
     Ok(())
 }
 
-fn render_to_string(entry: &Entry) -> Result<String> {
-    let mut cursor = Cursor::new(Vec::new());
-    render::html(entry, &mut cursor)?;
-    Ok(String::from_utf8(cursor.into_inner())?)
-}
-
 pub fn write_detail(entries: &mut Vec<Entry<'_>>) -> Result<()> {
-    #[derive(Serialize)]
-    struct Post<'a> {
-        html: &'a str,
-        prior_title: Option<&'a str>,
-        prior_slug: Option<&'a str>,
-        next_title: Option<&'a str>,
-        next_slug: Option<&'a str>,
-    }
+    use std::iter::once;
 
-    to_writer(
-        File::create(format!("assets/json/{}.json", entries[0].slug))?,
-        &Post {
-            html: render_to_string(&entries[0])?,
-            prior_title: None,
-            prior_slug: None,
-            next_title: Some(&entries[1].title),
-            next_slug: Some(&entries[1].slug),
-        },
-    )?;
+    let len = entries.len();
+    let iter = once((None, &entries[0], Some(&entries[1])))
+        .chain(
+            entries
+                .windows(3)
+                .map(|e| (Some(&e[0]), &e[1], Some(&e[2]))),
+        )
+        .chain(once((Some(&entries[len - 2]), &entries[len - 1], None)));
 
-    if entries.len() > 2 {
-        for posts in entries.windows(3) {
-            to_writer(
-                File::create(format!("assets/json/{}.json", posts[1].slug))?,
-                &Post {
-                    html: render_to_string(&posts[0])?,
-                    prior_title: Some(&posts[0].title),
-                    prior_slug: Some(&posts[0].slug),
-                    next_title: Some(&posts[2].title),
-                    next_slug: Some(&posts[2].slug),
-                },
-            )?;
-        }
-    }
-
-    to_writer(
-        File::create(format!(
+    for (prior, curr, next) in iter {
+        let mut cursor = Cursor::new(Vec::new());
+        render::html(curr, &mut cursor)?;
+        let obj = object! {
+            "html" => String::from_utf8(cursor.into_inner())?,
+            "prior_title" => prior.map(|p| p.title),
+            "prior_slug" => prior.map(|p| p.slug),
+            "next_title" => next.map(|p| p.title),
+            "next_slug" => next.map(|p| p.slug),
+        };
+        obj.write(&mut File::create(format!(
             "assets/json/{}.json",
-            entries[entries.len() - 1].slug
-        ))?,
-        &Post {
-            html: render_to_string(&entries[entries.len() - 1])?,
-            prior_title: Some(&entries[entries.len() - 2].title),
-            prior_slug: Some(&entries[entries.len() - 2].slug),
-            next_title: None,
-            next_slug: None,
-        },
-    )?;
+            curr.slug
+        ))?)?;
+    }
 
     Ok(())
 }
