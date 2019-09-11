86080a6b7406c99648d933ba7780e41373c99c1f
PoiScript <poiscript@gmail.com>
2019-09-11 13:34:34 +0800

    feat(web): serve link avatars from local

--- web/src/app/component/link/link.component.ts
+++ web/src/app/component/link/link.component.ts
@@ -1,84 +1,88 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
-import { Link } from '../../model';
+interface Link {
+  address: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({ templateUrl: './link.component.html' })
 export class LinkComponent {
   readonly links: Link[] = [
     {
       address: 'https://typeblog.net/',
-      avatar: 'https://github.com/PeterCxy.png?size=400',
+      avatar: 'PeterCxy',
       name: 'PeterCxy',
     },
     {
       address: 'https://fiveyellowmice.com/',
-      avatar: 'https://github.com/FiveYellowMice.png?size=400',
+      avatar: 'FiveYellowMice',
       name: 'FiveYellowMice',
     },
     {
       address: 'http://blog.lilydjwg.me/',
-      avatar: 'https://github.com/lilydjwg.png?size=400',
+      avatar: 'lilydjwg',
       name: '依云',
     },
     {
       address: 'https://blog.yoitsu.moe/',
-      avatar: 'https://github.com/KenOokamiHoro.png?size=400',
+      avatar: 'KenOokamiHoro',
       name: 'ヨイツの賢狼ホロ',
     },
     {
       address: 'https://blog.felixc.at/',
-      avatar: 'https://github.com/felixonmars.png?size=400',
+      avatar: 'felixonmars',
       name: 'Felix Yan',
     },
     {
       address: 'https://farseerfc.me/',
-      avatar: 'https://github.com/farseerfc.png?size=400',
+      avatar: 'farseerfc',
       name: 'farseerfc',
     },
     {
       address: 'https://tomli.blog/',
-      avatar: 'https://github.com/biergaizi.png?size=400',
+      avatar: 'biergaizi',
       name: '比尔盖子',
     },
     {
       address: 'https://void-shana.moe/',
-      avatar: 'https://github.com/VOID001.png?size=400',
+      avatar: 'VOID001',
       name: 'VOID001',
     },
     {
       address: 'https://marisa-kirisa.me/',
-      avatar: 'https://github.com/wengxt.png?size=400',
+      avatar: 'wengxt',
       name: 'CS Slayer',
     },
     {
       address: 'https://www.rabbittu.com/',
-      avatar: 'https://github.com/xiaoyu2016.png?size=400',
+      avatar: 'xiaoyu2016',
       name: 'NyanRabbit',
     },
     {
       address: 'https://blog.nfz.moe/',
-      avatar: 'https://github.com/neofelhz.png?size=400',
+      avatar: 'neofelhz',
       name: 'neoFelhz',
     },
     {
       address: 'https://sherlock-holo.github.io/',
-      avatar: 'https://github.com/Sherlock-Holo.png?size=400',
+      avatar: 'Sherlock-Holo',
       name: 'Sherlock Holo',
     },
     {
       address: 'https://blog.sukitsuki.com/',
-      avatar: 'https://github.com/tsuki.png?size=400',
+      avatar: 'tsuki',
       name: 'Tsuka Tsuki',
     },
     {
       address: 'https://ekyu.moe/',
-      avatar: 'https://github.com/Equim-chan.png?size=400',
+      avatar: 'Equim-chan',
       name: 'Equim',
     },
     {
       address: 'https://blog.nanpuyue.com',
-      avatar: 'https://github.com/nanpuyue.png?size=400',
+      avatar: 'nanpuyue',
       name: '南浦月',
     },
   ];
