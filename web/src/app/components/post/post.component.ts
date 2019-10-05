1a099d89658cc246580e5857bc75fc3d7af480e4
PoiScript <poiscript@gmail.com>
2019-10-05 16:33:53 +0800

    feat(web): insert link to AMP page

--- web/src/app/components/post/post.component.ts
+++ web/src/app/components/post/post.component.ts
@@ -1,9 +1,11 @@
 import {
+  Inject,
   OnInit,
   ChangeDetectorRef,
   Component,
   ViewEncapsulation,
 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
@@ -21,6 +23,7 @@ export class PostComponent implements OnInit {
     private route: ActivatedRoute,
     private titleService: Title,
     private cdRef: ChangeDetectorRef,
+    @Inject(DOCUMENT) private document: Document,
   ) {}
 
   ngOnInit() {
@@ -28,6 +31,13 @@ export class PostComponent implements OnInit {
       this.post = post;
       this.titleService.setTitle(post.title + '☆Solomon');
       this.cdRef.markForCheck();
+      let link = this.document.querySelector('link[rel="amphtml"]');
+      if (!link) {
+        link = this.document.createElement('link') as HTMLLinkElement;
+        link.setAttribute('rel', 'amphtml');
+        this.document.head.appendChild(link);
+      }
+      link.setAttribute('href', `https://blog.poi.cat/amp/${post.slug}`);
     });
   }
 
