07841a9d56a4f5da164ff0cbb945a65be3218bde
PoiScript <poiscript@gmail.com>
2019-10-16 00:18:19 +0800

    feat(web): update prerender.ts to use renderModule

--- web/scripts/prerender.ts
+++ web/scripts/prerender.ts
@@ -1,19 +1,15 @@
-/* tslint:disable:no-console */
-
 import 'zone.js/dist/zone-node';
 
-import { enableProdMode } from '@angular/core';
-import { renderModuleFactory } from '@angular/platform-server';
 import { outputFile, readFileSync, readJsonSync } from 'fs-extra';
 import { resolve } from 'path';
 import { minify } from 'html-minifier';
 
-const { AppServerModuleNgFactory } = require(resolve(__dirname, '../dist/server/main'));
+const postsJsonPath = resolve(__dirname, '../../assets/posts.json');
+const documentPath = resolve(__dirname, '../dist/app/index.html');
+const serverBundlePath = resolve(__dirname, '../dist/server/main');
 
-const posts = readJsonSync(resolve(__dirname, '../../assets/posts.json'));
-const document = readFileSync(resolve(__dirname,'../dist/app/index.html'), 'utf8');
-
-enableProdMode();
+const posts = readJsonSync(postsJsonPath);
+const document = readFileSync(documentPath, 'utf8');
 
 const pages = [
   ['/', 'index.html'],
@@ -26,16 +22,24 @@ for (const post of posts) {
   pages.push([`/post/${post.slug}`, `post/${post.slug}.html`]);
 }
 
-Promise.all(
-  pages.map(([url, path]) =>
-    renderModuleFactory(AppServerModuleNgFactory, { url, document })
-      .then(html =>
-        minify(html, {
-          collapseWhitespace: true,
-          removeComments: true,
-          minifyCSS: true,
-        }),
-      )
-      .then(html => outputFile(resolve(__dirname, '../dist/app/', path), html)),
-  ),
-).then(res => console.log(`Pre-rendered ${res.length} files`));
+(async () => {
+  const { AppServerModule, renderModule } = await import(serverBundlePath);
+
+  const res = await Promise.all(
+    pages.map(([url, path]) =>
+      renderModule(AppServerModule, { url, document })
+        .then(html =>
+          minify(html, {
+            collapseWhitespace: true,
+            removeComments: true,
+            minifyCSS: true,
+          }),
+        )
+        .then(html =>
+          outputFile(resolve(__dirname, '../dist/app/', path), html),
+        ),
+    ),
+  );
+
+  console.log(`Pre-rendered ${res.length} files`);
+})();
--- web/src/main.server.ts
+++ web/src/main.server.ts
@@ -7,3 +7,4 @@ if (environment.production) {
 }
 
 export { AppServerModule } from './app/app.server.module';
+export { renderModule } from '@angular/platform-server';
